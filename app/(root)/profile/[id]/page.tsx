fe2fe570cdabb5c32fe7636ff8ea15bc3f0663dc
abhi162002 <110338080+abhi162002@users.noreply.github.com>
2023-08-25 22:28:13 +0530

    Update page.tsx
    Below Bio, Props
    type={""}

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -27,7 +27,6 @@ async function Page({ params }: { params: { id: string } }) {
                 username={userInfo.username}
                 imgUrl={userInfo.image}
                 bio={userInfo.bio}
-                type={""}
             />
 
             <div className='mt-9'>
